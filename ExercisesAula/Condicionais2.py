c53446ef86c37e4794feb721cf74109bcf17fe5a
scartIsa <125595428+scartIsa@users.noreply.github.com>
2023-03-15 23:41:36 +0000

    condicionas2/ aula completed

---
+++ ExercisesAula/Condicionais2.py
@@ -0,0 +1,6 @@
+A = int(input("pls write a random number: "))
+B = int(input("pls write another random number: "))
+if A > B:
+    print("A is the larger number")
+else:
+    print("B is the larger number")
