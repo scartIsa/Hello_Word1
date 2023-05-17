3ac9415c91a36cf30443a5410c91401c382755d0
scartIsa <125595428+scartIsa@users.noreply.github.com>
2023-05-17 23:52:47 +0000

    novo (aula)

---
+++ ExercisesAula/carro.py
@@ -0,0 +1,12 @@
+class Carro():
+    def __init__(self, nome, modelo, cor, ano, buzina):
+        self.nome = nome
+        self.modelo = modelo
+        self.cor = cor
+        self.ano = ano
+        self.buzina = buzina
+    def print(self):
+        print(f"O nome é {self.nome} o modelo é {self.modelo} a cor é {self.cor} e o ano é {self.ano}, e tem uma linda {self.buzina}!!!!")
+        
+
+
