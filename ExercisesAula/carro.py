0ac05635bf386069588eba8f3ad1d9bda6189c03
scartIsa <125595428+scartIsa@users.noreply.github.com>
2023-05-18 00:05:34 +0000

    fusca rosa :)

--- ExercisesAula/carro.py
+++ ExercisesAula/carro.py
@@ -7,6 +7,6 @@ class Carro():
         self.buzina = buzina
     def print(self):
         print(f"O nome é {self.nome} o modelo é {self.modelo} a cor é {self.cor} e o ano é {self.ano}, e tem uma linda {self.buzina}!!!!")
-        
 
-
+meu_carro = Carro("Fusca", "1500", "rosa", 1975, "buzina")
+meu_carro.print()
